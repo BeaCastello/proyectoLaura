97c9ab7c05eb0002ff8cd5b3c8db3f6fbcf5c6b3
beatriz <designerbrm@gmail.com>
2024-12-06 23:20:00 +0100

    nuevos cambios

--- script.js
+++ script.js
@@ -12,3 +12,37 @@ function toggleContent() {
         button.textContent = 'Mostrar menos';
     }
 }
+
+//¿Qué es un arraigo?
+
+function changeContent(title, imageSrc, title1, description) {
+    const titleElement = document.getElementById('title');
+    const imageElement = document.getElementById('left-image');
+    const titleElementDuplic = document.getElementById('title1');
+    const descriptionElement = document.getElementById('description');
+
+
+    // Actualiza el titulo
+    titleElement.innerHTML = title;
+
+    // Actualiza la imagen
+    imageElement.src = imageSrc;
+    imageElement.alt = title1;
+
+
+    // Actualiza el título y la descripción
+    titleElementDuplic.innerHTML = title1;
+    descriptionElement.innerHTML = description;
+
+    // Inserta el título nuevamente después de la imagen
+    const duplicateTitle = document.createElement('h2');
+    duplicateTitle.innerHTML = title1;
+    duplicateTitle.className = 'roots-title';
+
+    // Inserta el nuevo título antes de la descripción y después de la imagen
+    if (textContainer.querySelector('.title1')) {
+        textContainer.querySelector('.title1').remove();
+    }
+    duplicateTitle.classList.add('title1');
+    textContainer.insertBefore(title1, descriptionElement);
+}
